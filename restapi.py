279d47cac454b7ff136e5f4282c8e5d185434da5
olan <olan@olan.com>
2020-03-04 15:37:40 +0000

    renamed arrays for returned json files to be valid attribute names

--- restapi.py
+++ restapi.py
@@ -18,14 +18,14 @@ def hello():
 def carMakes():
     mycursor.execute("SELECT * FROM carMakes")
     myresult = mycursor.fetchall()
-    return jsonify({"Car Makes": [make[0] for make in myresult]})
+    return jsonify({"makes": [make[0] for make in myresult]})
 
 @app.route("/car_models/<string:make>")
 def carModels(make):
     sql = "SELECT model FROM carModels WHERE make = '%s'" % make
     mycursor.execute(sql)
     myresult = mycursor.fetchall()
-    return jsonify({"Car Model": [model[0] for model in myresult]})
+    return jsonify({"models": [model[0] for model in myresult]})
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5001")
